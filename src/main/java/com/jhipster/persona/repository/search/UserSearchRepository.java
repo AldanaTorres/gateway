620e10aa9fc9e72c47729a5c95881795c8b41a5e
Aldana Torres <aldanaatorres@gmail.com>
2020-11-10 22:40:19 -0300

    Initial version of gateway generated by JHipster-6.10.4

---
+++ src/main/java/com/jhipster/persona/repository/search/UserSearchRepository.java
@@ -0,0 +1,11 @@
+package com.jhipster.persona.repository.search;
+
+import com.jhipster.persona.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+
+/**
+ * Spring Data Elasticsearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, Long> {
+}
